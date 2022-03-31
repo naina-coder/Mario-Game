03a0cb043a8c8a43ff43241319bdf438d0ad2ec3
naina-coder <82169500+naina-coder@users.noreply.github.com>
2022-03-31 12:26:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function preload() {
+
+	mario_jump = loadSound("jump.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	world_start = loadSound("world_start.wav");
+	
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
+	video = createCapture(VIDEO);
+ 	video.size(800,400);
+
+	 video.parent('game_console');
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+
+	instializeInSetup(mario);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+  
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	  console.log("noseX = " + noseX +", noseY = " + noseY);
+	}
+  }
+
+  
+function draw() {
+	game();
+}
+
+
+
+
+
+
